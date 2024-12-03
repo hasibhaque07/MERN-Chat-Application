3131780ad9936e9005174208d10fd5c8db065b53
Hasib <hasibhaque@gmail.com>
2024-12-03 20:11:46 +0600

    updated

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,7 @@
 import dotenv from "dotenv";
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
@@ -11,6 +13,11 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}))
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
